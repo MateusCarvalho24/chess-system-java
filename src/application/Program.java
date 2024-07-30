4de7270992553331b4fc39701fc57eb4a596350d
Mateus Carvalho <mateus.carvalho2411@gmail.com>
2024-07-30 18:58:10 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,12 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = scan.nextLine().toUpperCase();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch (ChessException error) {
 				System.out.println(error.getMessage());
